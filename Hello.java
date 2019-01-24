d8ff6ab4dba1f61e5ada614478b65ce34faaa575
Jhalak Naik <jhalak.naik@gmail.com>
2019-01-24 12:09:12 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello{
     public static void main(String args[])
         {
 	    System.out.print("Hello world! \n");
-	    System.out.print("Hello  ! \n");
+	    System.out.print("Hello Jhalak ! \n");
 	    System.exit(0);
         }
 }
